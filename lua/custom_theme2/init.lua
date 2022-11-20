00fa1f6ce7ac54b0151e775c4c91a5d4f3c61949
EzequielLo <68113918+EzequielLo@users.noreply.github.com>
2022-11-20 20:44:18 +0100

    Update init.lua

--- lua/custom_theme2/init.lua
+++ lua/custom_theme2/init.lua
@@ -35,7 +35,7 @@ local highlights = {
   { hg = 'Repeat', fg = colors.purple }, --for, do, while, etc.
   { hg = 'Label', fg = colors.purple }, --case, default, etc.
   { hg = 'Operator', fg = colors.foreground }, --sizeof", "+", "*", etc.
-  { hg = 'Keyword', fg = colors.purple }, --any other keyword
+  { hg = 'Keyword', fg = colors.red }, --any other keyword
   { hg = 'Exception', fg = colors.purple }, --try, catch, throw
   { hg = 'PreProc', fg = colors.red }, --generic Preprocessor
   { hg = 'Include', fg = colors.foreground}, --preprocessor #include
